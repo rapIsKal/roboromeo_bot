75a34eccfb2b97d5e5723d9cc832bc1b3aabb7da
Vlad Gorelov <36513126+rapIsKal@users.noreply.github.com>
2022-08-23 21:43:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,36 @@
-import logging
 import os
 
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.utils.executor import start_webhook
+from aiogram import Bot, Dispatcher, types, executor
 
 TOKEN = os.getenv('BOT_TOKEN')
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.answer(message.text)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
